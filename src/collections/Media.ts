950cc9022e7dd2f383e1aebd98cbf4c3904e024c
Daniel Shi <danielshi96@gmail.com>
2025-07-08 14:28:26 -0700

    Updating project

--- src/collections/Media.ts
+++ src/collections/Media.ts
@@ -1,4 +1,17 @@
 import type { CollectionConfig } from 'payload'
+import s3Adapter from 'payload-s3-upload'
+
+
+const adapter = s3Adapter({
+  config: {
+    credentials: {
+      accessKeyId: process.env.S3_ACCESS_KEY_ID || '',
+      secretAccessKey: process.env.S3_SECRET_ACCESS_KEY || '',
+    },
+    region: process.env.S3_REGION || '',
+  },
+  bucket: process.env.S3_BUCKET || '',
+})
 
 export const Media: CollectionConfig = {
   slug: 'media',
@@ -6,15 +19,16 @@ export const Media: CollectionConfig = {
     read: () => true,
   },
   upload: {
+    adapter,
     mimeTypes: [
       'image/jpeg',
       'image/png',
       'image/gif',
       'video/mp4',
       'video/webm',
-      'video/quicktime', // for .mov files
+      'video/quicktime',
     ],
-    adminThumbnail: 'filename', // fallback preview for video
+    adminThumbnail: ({ doc }) => doc?.url, // show preview of image or video
   },
   fields: [
     {
