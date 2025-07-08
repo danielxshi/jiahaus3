08b055ad2b324fa546ea41d4185e133ff9ba4b8c
Daniel Shi <danielshi96@gmail.com>
2025-07-08 15:54:15 -0700

    Deployed payload

--- src/collections/Media.ts
+++ src/collections/Media.ts
@@ -1,14 +1,4 @@
 import type { CollectionConfig } from 'payload'
-import s3Adapter from 'payload-s3-upload'
-import { S3Client } from '@aws-sdk/client-s3'
-
-const s3Client = new S3Client({
-  region: process.env.S3_REGION || '',
-  credentials: {
-    accessKeyId: process.env.S3_ACCESS_KEY_ID || '',
-    secretAccessKey: process.env.S3_SECRET_ACCESS_KEY || '',
-  },
-})
 
 export const Media: CollectionConfig = {
   slug: 'media',
@@ -16,11 +6,7 @@ export const Media: CollectionConfig = {
     read: () => true,
   },
   upload: {
-    disableLocalStorage: true,
-    adapter: s3Adapter({
-      client: s3Client,
-      bucket: process.env.S3_BUCKET || '',
-    }) as unknown as string,
+    disableLocalStorage: true, // this enables S3 upload
     mimeTypes: [
       'image/jpeg',
       'image/png',
@@ -29,7 +15,7 @@ export const Media: CollectionConfig = {
       'video/webm',
       'video/quicktime',
     ],
-    adminThumbnail: ({ doc }) => doc?.url || '',
+    adminThumbnail: ({ doc }) => (typeof doc?.url === 'string' ? doc.url : null),
   },
   fields: [
     {
